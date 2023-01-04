db860fee3a161b15bd7041fcba9042f16cfb1b3a
yaukhutadzianis@gmail.com <yaukhutadzianis@gmail.com>
2023-01-04 13:57:49 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Header from "./components/Header/Header";
+import Todo from "./components/Todo/Todo";
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Todo />
     </div>
   );
 }
---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Todo() {
+  return (
+    <div>
+        <div className="container py-lg-5">
+            <h1>Todo</h1>
+            <input class="form-control" type="text" value="Readonly input here..." aria-label="input"></input>
+        </div>
+    </div>
+  )
+}
+
+export default Todo
